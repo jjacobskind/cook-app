adc599ef222b1839fe09b2a1107ca49215e45705
Jeremy <jeremy.jacobskind@gmail.com>
2014-11-08 16:15:41 -0500

    Moved entire search process into search module

--- server/api/source/source.controller.js
+++ server/api/source/source.controller.js
@@ -137,118 +137,15 @@ exports.recommendedSearch = function(req, res){
 // Searches Fork2Food's API
 exports.getRecipes = function(req, res) {
   var search_terms = req.body.search;
+  var recommended = req.body.recommended;
+  var id = req.body.id;
   var unique_terms = _.unique(search.wordProcess(search_terms));
   var url = 'http://food2fork.com/api/search?' + querystring.stringify({
     'key':f2fkey,
     'q': search_terms
   });
 
-  var search_info ={};
-
-  // Get user's skills
-  User.findById(req.body.id).exec()
-    .then(function(user){
-      if(!req.body.recommended){
-        user.addSearchTerms(unique_terms);
-      }
-      return user.skills.map(function(item){
-        return String(item.skill_tag);
-      })
-    })
-    .then(function(user_skills){
-
-      // Filter recipe results by user's skills
-      Recipe.find({tags: {$in: user_skills} }).exec()
-        .then(function(recipes){
-          var filtered_recipes = recipes.filter(function(item){
-            var i = item.tags.length;
-            while(i--){
-              if(user_skills.indexOf(String(item.tags[i]))===-1) {
-                return false;
-              }
-            }
-            return true;
-          });
-          return filtered_recipes;
-        })
-
-        // Filter remaining results by search terms
-        .then(function(filtered_recipes){
-          return filtered_recipes.filter(function(item){
-            var i = item.stemmed_words.length;
-            while(i--){
-              if(unique_terms.indexOf(String(item.stemmed_words[i]))!==-1) {
-                return true;
-              }
-            }
-            return false;
-          });
-        })
-
-          // Create an object containing an array of populate functions
-          // And an array for results to be pushed into
-        .then(function(completely_filtered_recipes){
-          var pop_obj = {functions:[], values:[]};
-          pop_obj.functions = completely_filtered_recipes.map(function(item){
-            var this_func =  function(cb){
-              item.populate('tags', function(err1, populated_item){
-                pop_obj.values.push(populated_item);
-                cb();
-              });
-            };
-            return this_func;
-          });
-          return pop_obj;
-        })
-
-        // populate the recipe result skill tags
-        .then(function(pop_obj) {
-          Q.nfcall(async.parallel, pop_obj.functions)
-            .then(function(){
-                return res.send(pop_obj.values);
-            });
-        })
-    });
-    return false; //Temporary cutoff for testing purposes
-
-  // Loads array of domains that are already registered in selector tool
-  // These arrays will be used to determine whether a result can be displayed
-  // Or whether the domain should be added to the selector tool
-  Source.find({}).exec()
-
-    // Add two arrays to search_info
-    // First array contains the domain url
-    // Second array contains a boolean value indicating whether a selector has been picked
-    .then(search.getDomains)
-
-    // Attach Food2Fork API URL containing search parameters
-    .then(function(ret_info){
-      search_info = ret_info;
-      search_info.cur_url = url;
-      return search_info;
-    })
-
-    // Add a 'titles' array to search_info
-    // Each array element consists of an object containing the name and URL of a recipe
-    .then(function(search_info){
-      Q.nfcall(request, search_info.cur_url)
-        .then(function(res){
-          search_info.titles = search.searchRecipes(search_info, res[0].body);
-          return search_info;
-        })
-
-        // Queries database for all skill tags
-        // Populates base_tags with array of all base words for tags
-        // Populates display_tags with array of all display words for tags
-        // Arrays are populated simultaneously, so indices match
-        .then(function(search_info){
-          Tag.find({}).exec()
-            .then(function(tagArr){
-              search.getTags(tagArr, res, search_info);
-            });
-        })
-        .fail(function(err){ console.log(err); });
-    });
+  search.startSearch(res, search_terms, unique_terms, id, url, recommended);
 };
 
 // Saves Source data in seed file as backup
